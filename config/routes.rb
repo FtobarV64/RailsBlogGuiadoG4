74ea98875ecc7aec44f80e74b8b4eafb0b315ca3
FtobarV64 <ftobarv@gmail.com>
2022-05-12 21:55:30 -0400

    various commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # resources :posts
+  # resources :posts, only: [:index, :show, :create, :update, :destroy]
+  resources :posts, except: [:new, :edit]
+
 end
